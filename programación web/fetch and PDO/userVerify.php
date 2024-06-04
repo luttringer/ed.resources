6bdc1d04e5909e7e480f3bc5677271b36f7be171
Christian Menendez Luttringer <luttringerezequiel@gmail.com>
2024-06-04 12:14:20 -0300

    cargo documentacion en userVerify.php e index.html

--- programación web/fetch and PDO/userVerify.php
+++ programación web/fetch and PDO/userVerify.php
@@ -1,6 +1,13 @@
 <?php
     header('Content-Type: application/json');
 
+    /*
+        documentacion: configuro el header de php para recibir y enviar archivos en formato json. luego consulto con un if si existen datos recibidos por POST (usr y pass)
+            si existen los cargo en dos variables. dentro de un try (intenta correr el codigo, si hay error no se rompe sino que lo registra) configuro una conexion a bd mediante PDO
+            y creo una consulta que evalua si existe un usuario en la tabla usuarios, con el user y pass que recibi. recorro la respuesta de la consulta y si devolvio 1 o mas, entonces devuelvo un json
+            con el atributo exist en true (en js luego esto se evalua), sino en false.
+    */
+
     if (isset($_POST['user']) && isset($_POST['pass'])) 
     {
         $usuario = $_POST['user'];
